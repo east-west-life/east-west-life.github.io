a729bda6b42f1a8451a8e57f07b7632d28e1fc73
Stephen Cheng <jetrobert19@gmail.com>
2024-03-16 10:01:59 -0400

    0316

--- components/header-2.js
+++ components/header-2.js
@@ -43,11 +43,11 @@ headerTemplate.innerHTML = `
             <li><a href="../about-us.html" target="_blank">About us</a></li>
           </ul>
         </li>
+        <li><a href="../index-cn.html"><span class="glyphicon"></span> 中文</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <!--<li><a href="#"><span class="glyphicon glyphicon-user"></span> Register</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
-        <li><a href="../index-cn.html"><span class="glyphicon"></span> 中文</a></li>
       </ul> 
     </div>
   </div>
--- components/header-cn.js
+++ components/header-cn.js
@@ -43,11 +43,11 @@ headerTemplate.innerHTML = `
             <li><a href="about-us.html" target="_blank">关于我们</a></li>
           </ul>
         </li>
+        <li><a href="index.html"><span class="glyphicon"></span> English</a></li>
       </ul>
      <ul class="nav navbar-nav navbar-right">
         <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> 注册</a></li>
         <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> 登录</a></li> -->
-        <li><a href="index.html"><span class="glyphicon"></span> English</a></li>
       </ul>
     </div>
   </div>
