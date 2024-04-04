e4d6eff1304e25b72d451d5c262913408d43a231
Stephen Cheng <jetrobert19@gmail.com>
2024-04-04 11:49:18 -0400

    0404

--- components/header.js
+++ components/header.js
@@ -10,47 +10,46 @@ headerTemplate.innerHTML = `
   </style>
   <nav class="navbar navbar-default">
     <div class="container-fluid">
-      <div class="navbar-header">
-        <ul class="nav navbar-nav">
-          <li><a href="index.html"><span class="glyphicon glyphicon-home"></span> EWL</a></li>
-          <li class="dropdown">
-            <button class="dropbtn">Explore
-              <span class="caret"></span>
-            </button>
-            <ul class="dropdown-content">
-                <li><a href="life/australia.html">Australia</a></li>
-                <li><a href="life/canada.html">Canada</a></li>
-                <li><a href="life/japan.html">Japan</a></li>
-                <li><a href="life/malaysia.html">Malaysia</a></li>
-                <li><a href="life/newzealand.html">New Zealand</a></li>
-                <li><a href="life/singapore.html">Singapore</a></li>
-                <li><a href="life/thailand.html">Thailand</a></li>
-                <li><a href="life/uk.html">UK</a></li>
-                <li><a href="life/usa.html">USA</a></li>
-                <li><a href="life/southeast-asia.html">SEA</a></li>
-                <li><a href="life/asia.html">Asia</a></li>
-                <li><a href="life/world.html">World</a></li>
-                <li><a href="life/they.html">They</a></li>
-            </ul>
-          </li>
-          <li class="dropdown">
-            <button class="dropbtn">More
-              <span class="caret"></span>
-            </button>
-            <ul class="dropdown-content">
-              <li><a href="web.html" target="_blank">Websites</a></li>
-              <li><a href="project.html" target="_blank">Project</a></li>
-              <li><a href="https://studymax.org/" target="_blank">Education</a></li>
-              <li><a href="about-us.html" target="_blank">About us</a></li>
-            </ul>
-          </li>
-          <li><a href="index-cn.html">中文</a></li>
-        </ul>
-      <ul class="nav navbar-nav navbar-right">
-          <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Register</a></li>
-          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
-        </ul>
-      </div>
+      <!-- <div class="navbar-header"></div> -->
+      <ul class="nav navbar-nav">
+        <li><a href="index.html"><span class="glyphicon glyphicon-home"></span> EWL</a></li>
+        <li class="dropdown">
+          <button class="dropbtn">Explore
+            <span class="caret"></span>
+          </button>
+          <ul class="dropdown-content">
+              <li><a href="life/australia.html">Australia</a></li>
+              <li><a href="life/canada.html">Canada</a></li>
+              <li><a href="life/japan.html">Japan</a></li>
+              <li><a href="life/malaysia.html">Malaysia</a></li>
+              <li><a href="life/newzealand.html">New Zealand</a></li>
+              <li><a href="life/singapore.html">Singapore</a></li>
+              <li><a href="life/thailand.html">Thailand</a></li>
+              <li><a href="life/uk.html">UK</a></li>
+              <li><a href="life/usa.html">USA</a></li>
+              <li><a href="life/southeast-asia.html">SEA</a></li>
+              <li><a href="life/asia.html">Asia</a></li>
+              <li><a href="life/world.html">World</a></li>
+              <li><a href="life/they.html">They</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <button class="dropbtn">More
+            <span class="caret"></span>
+          </button>
+          <ul class="dropdown-content">
+            <li><a href="web.html" target="_blank">Websites</a></li>
+            <li><a href="project.html" target="_blank">Project</a></li>
+            <li><a href="https://studymax.org/" target="_blank">Education</a></li>
+            <li><a href="about-us.html" target="_blank">About us</a></li>
+          </ul>
+        </li>
+        <li><a href="index-cn.html">中文</a></li>
+      </ul>
+      <!-- <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> Register</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul> -->
     </div>
   </nav>
 `;
